2e536970810251162c9ccb082c0a5381c27dfcaa
samuel asare <samuelaboahasare@gmail.com>
2025-07-21 09:46:51 +0000

    task: In your existing Django project, enhance the relationship_app by adding new views that display information about books and libraries. Implement both function-based and class-based views to handle these displays and configure the URL patterns to route these views correctly.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,20 @@
-from django.urls import path
+# from django.urls import path
 
-from . import views
+# from . import views
+
+# app_name = 'relationship_app'
+
+# urlpatterns = [
+#     path('books/', views.list_books, name='list_books'),
+#     path('library/<int:library_id>/', views.LibraryDetailView.as_view(), name='library_detail'),
+# ]
+
+from django.urls import path
+from .views import list_books, LibraryDetailView
 
 app_name = 'relationship_app'
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:library_id>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:library_id>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
