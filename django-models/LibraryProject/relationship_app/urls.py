6ae5a1231d9004cc908cf5d64fc275897f2e1c23
samuel asare <samuelaboahasare@gmail.com>
2025-07-21 10:44:58 +0000

    task: In the relationship_app of your Django project, extend the existing Book model to include custom permissions. You will then update the views to enforce these permissions, ensuring that only authorized users can perform certain actions.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,12 +16,19 @@ from django.contrib.auth.views import LoginView, LogoutView
 app_name = 'relationship_app'
 
 urlpatterns = [
+    # VIEWS FOR ALL USERS
     path('books/', views.list_books, name='list_books'),
     path('library/<int:library_id>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    # AUTHENTICATION
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    # DASHBOARD BASE ON ROLES
     path('admin-dashboard/', views.admin_view, name='admin_view'),
     path('librarian-dashboard/', views.librarian_view, name='librarian_view'),
     path('member-dashboard/', views.member_view, name='member_view'),
+    # PERMISSIONS FOR THE ROLES OF USERS
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
+    path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
 ]
