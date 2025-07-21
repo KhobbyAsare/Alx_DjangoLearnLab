b51268f889481fc73cf136cd209369d0dc35b76b
samuel asare <samuelaboahasare@gmail.com>
2025-07-21 09:43:43 +0000

    task: In your existing Django project, enhance the relationship_app by adding new views that display information about books and libraries. Implement both function-based and class-based views to handle these displays and configure the URL patterns to route these views correctly.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Library, Book
 
-# Create your views here.
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+# Class-based view for displaying a library's detail and its books
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+    pk_url_kwarg = 'library_id'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Fetch all books associated with this library
+        context['books'] = self.object.books.all()
+        return context
