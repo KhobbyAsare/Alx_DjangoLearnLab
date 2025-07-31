e4112d175a755241c9cf17afff98494412ca6a0e
samuel asare <samuelaboahasare@gmail.com>
2025-07-31 12:47:55 +0000

    task: In this task, you will replace the simple list view created previously with a full set of CRUD operations using DRF’s ViewSets. This will allow clients to not only retrieve but also create, update, and delete books via your API.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,23 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework import viewsets
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        
