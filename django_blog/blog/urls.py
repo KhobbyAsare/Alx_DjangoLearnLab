df73347241e6ce39a8d521d46602e13f5e36d113
samuel asare <samuelaboahasare@gmail.com>
2025-08-14 22:47:06 +0000

    task: In this task, you will expand the django_blog project by adding a comment feature to the blog posts. Users will be able to read comments, and authenticated users will have the ability to post, edit, and delete their comments.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
     
     # Comment CRUD URLs
-    path('posts/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='add_comment'),
-    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='edit_comment'),
-    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),
+    path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='add_comment'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='edit_comment'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),
 ]
