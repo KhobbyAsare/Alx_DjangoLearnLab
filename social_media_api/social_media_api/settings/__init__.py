dce629a955523b29ce51272634baebb1fe0bcf62
samuel asare <samuelaboahasare@gmail.com>
2025-08-22 05:33:00 +0000

    task: Finalize your social_media_api project for public availability by deploying it to a production server. This will allow real-world testing and interaction, making the project a significant addition to your portfolio.

--- social_media_api/social_media_api/settings/__init__.py
+++ social_media_api/social_media_api/settings/__init__.py
@@ -1,17 +1,9 @@
 """
 Settings package initialization.
-Automatically loads the appropriate settings module based on the environment.
+
+NOTE: This package is currently disabled.
+Django is using the main social_media_api/settings.py file instead.
 """
 
-import os
-
-# Determine which settings to use based on environment
-environment = os.environ.get('DJANGO_ENVIRONMENT', 'development')
-
-if environment == 'production':
-    from .production import *
-elif environment == 'development':
-    from .development import *
-else:
-    # Default to development settings
-    from .development import *
+# Package disabled - using main settings.py file
+pass
